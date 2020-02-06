ffa3127c25fcc13a32fa50d61def9c5ec5efc2ba
Branden Butler <bwtbutler@hotmail.com>
2020-02-05 21:36:20 -0600

    Merge functionality from standard setup.py and CMake-compatible setup.py

--- setup.py
+++ setup.py
@@ -1,23 +1,36 @@
 #!/usr/bin/env python3
 
+import os
 from setuptools import setup
 
-setup(name='CMakeDoc',
-      version='1.0',
-      description='Documentation Generator for the CMake language',
-      author='Branden Butler',
-      author_email='bwtbutler@hotmail.com',
-      url='https://github.com/CMakePP/CMakeDoc',
-      packages=['cmakedoc', 'cmakedoc.parser'],
-      entry_points={
+#Split the prefix and var so they aren't automatically replaced by CMake
+prefix = "$"
+var = '{CMAKE_CURRENT_SOURCE_DIR}'
+dir='${CMAKE_CURRENT_SOURCE_DIR}'
+
+config = {
+      'name': 'CMakeDoc',
+      'version': '1.0',
+      'description': 'Documentation Generator for the CMake language',
+      'author': 'Branden Butler',
+      'author_email': 'bwtbutler@hotmail.com',
+      'url': 'https://github.com/CMakePP/CMakeDoc',
+      'packages': ['cmakedoc', 'cmakedoc.parser'],
+      'entry_points': {
         'console_scripts': [
             'cmakedoc = cmakedoc:main'
         ]
       },
-      package_data = {
+      'package_data':  {
            "": ["*.g4", "*.interp", "*.tokens"]
       },
-      install_requires = [
+      'install_requires':  [
           "antlr4-python3-runtime",
       ]
-     )
+     }
+
+if not ((prefix + var) == dir):
+	config.update({'package_dir': { 'cmakedoc':  os.path.join(dir, "cmakedoc")}})
+	#print(f"UPDATING CONFIG: {config}")
+
+setup(**config)
