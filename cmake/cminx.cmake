808bccf66a4f270438d7f2f04aa8f5dd33033713
ryan <ryanmrichard1@gmail.com>
2021-10-26 13:56:30 -0500

    undo forray into licensing

--- cmake/cminx.cmake
+++ cmake/cminx.cmake
@@ -1,20 +1,3 @@
-################################################################################
-#                                                                              #
-# Copyright 2021 CMakePP Team                                                  #
-#                                                                              #
-# Licensed under the Apache License, Version 2.0 (the "License");              #
-# you may not use this file except in compliance with the License.             #
-# You may obtain a copy of the License at                                      #
-#                                                                              #
-#     http://www.apache.org/licenses/LICENSE-2.0                               #
-#                                                                              #
-# Unless required by applicable law or agreed to in writing, software          #
-# distributed under the License is distributed on an "AS IS" BASIS,            #
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.     #
-# See the License for the specific language governing permissions and          #
-# limitations under the License.                                               #
-#                                                                              #
-################################################################################
 include_guard()
 
 set(CMAKEDOC_SRC "${CMAKE_CURRENT_SOURCE_DIR}" CACHE FILEPATH "Location of CMinx")
@@ -52,3 +35,4 @@ endfunction()
 function(cminx_add_docs_target _cad_doc_dir _cad_output_dir _cad_sphinx_target)
 	add_custom_target(docs COMMAND make "${_cad_sphinx_target}" "BUILDDIR=${_cad_output_dir}" "SPHINXBUILD=${VENV_PYTHON_EXECUTABLE} -m sphinx" WORKING_DIRECTORY "${_cad_doc_dir}")
 endfunction()
+
