2e03cbc120083e8df3f8a86019f16f5a9b548f87
Branden Butler <bwtbutler@hotmail.com>
2022-07-06 10:43:06 -0500

    Add settings classes

--- cminx/config.py
+++ cminx/config.py
@@ -1,4 +1,6 @@
 import confuse
+from typing import List
+from dataclasses import dataclass
 
 config_template = {
     "input": {
@@ -24,3 +26,48 @@ config_template = {
     }
 
 }
+
+
+@dataclass
+class InputSettings:
+    include_undocumented_functions: bool = True
+    include_undocumented_macros: bool = True
+    include_undocumented_classes: bool = True
+    include_undocumented_attributes: bool = True
+    include_undocumented_members: bool = True
+    recursive: bool = False
+
+
+@dataclass
+class OutputSettings:
+    directory: str = None
+
+
+@dataclass
+class LoggingSettings:
+    level: str = "INFO"
+
+
+@dataclass
+class RSTSettings:
+    file_extensions_in_titles: bool = False
+    file_extensions_in_modules: bool = False
+    prefix: str = None
+    module_path_separator: str = "."
+    headers: List[str] = ('#', '*', '=', '-', '_', '~', '!', '&', '@', '^')
+
+
+@dataclass
+class Settings:
+    input: InputSettings
+    output: OutputSettings
+    logging: LoggingSettings
+    rst: RSTSettings
+
+
+def dict_to_settings(input_dict: dict):
+    input_settings = InputSettings(**input_dict["input"])
+    output_settings = OutputSettings(**input_dict["output"])
+    logging_settings = LoggingSettings(**input_dict["logging"])
+    rst_settings = RSTSettings(**input_dict["rst"])
+    return Settings(input_settings, output_settings, logging_settings, rst_settings)
