732771e74749cac652f8987abe52e680c5e404dd
Branden Butler <bwtbutler@hotmail.com>
2022-07-28 16:12:02 -0500

    Update documentation_types class docstrings

--- cminx/documentation_types.py
+++ cminx/documentation_types.py
@@ -2,7 +2,7 @@ from abc import ABC, abstractmethod
 from dataclasses import dataclass, field
 from enum import Enum
 
-from rstwriter import RSTWriter
+from .rstwriter import RSTWriter, Directive
 
 
 class VarType(Enum):
@@ -15,16 +15,29 @@ class DocumentationType(ABC):
     """
     This is the base class for all documentation types. It defines a single
     abstract process() method that is used to write the documentation RST
-    to a given RSTWriter.
+    to a given :class:`RSTWriter`.
     """
 
     @abstractmethod
     def process(self, writer: RSTWriter):
+        """
+        Processes the data stored in this documentation type,
+        converting it to RST form by using the given RST writer.
+
+        :param writer: RSTWriter instance that the documentation
+        will be added to.
+        """
         pass
 
 
 @dataclass
 class FunctionDocumentation(DocumentationType):
+    """
+    This class serves as the representation of a documented
+    CMake function definition. It converts the function definition
+    directly to a Sphinx :code:`function` directive.
+    """
+
     function: str
     params: list[str]
     doc: str
@@ -37,6 +50,12 @@ class FunctionDocumentation(DocumentationType):
 
 @dataclass
 class MacroDocumentation(DocumentationType):
+    """
+    This class serves as the representation of a documented
+    CMake macro definition. It converts the macro definition
+    directly to a Sphinx :code:`function` directive and adds a
+    :code:`warning` directive specifying that it is a macro.
+    """
     macro: str
     params: list[str]
     doc: str
@@ -52,6 +71,17 @@ class MacroDocumentation(DocumentationType):
 
 @dataclass
 class VariableDocumentation(DocumentationType):
+    """
+    This class serves as the representation of a documented
+    CMake variable definition (:code:`set()` command). It can only
+    recognize the CMake primitives :code:`str` and :code:`list`, as
+    well as the :code:`UNSET` subcommand.
+
+    This class translates the definition into a Sphinx
+    :code:`data` directive, with a :code:`Default value` field
+    and a :code:`type` field.
+    """
+
     varname: str
     type: VarType
     value: str
@@ -74,6 +104,15 @@ class VariableDocumentation(DocumentationType):
 
 @dataclass
 class GenericCommandDocumentation(DocumentationType):
+    """
+    This class represents any documented command that does
+    not fit into any of the other documentation types.
+
+    All this class does is translate the documented
+    command into a Sphinx :code:`function` directive with
+    a warning that it is a command invocation and not
+    a definition.
+    """
     name: str
     params: list[str]
     doc: str
@@ -89,6 +128,14 @@ class GenericCommandDocumentation(DocumentationType):
 
 @dataclass
 class TestDocumentation(DocumentationType):
+    """
+    This class provides support for documenting
+    CMakeTest tests. It translates the test definition
+    into a Sphinx :code:`function` definition with an
+    EXPECTFAIL parameter if the test is expected to fail
+    and a warning stating that it is a test and should not
+    be called manually.
+    """
     name: str
     expect_fail: bool
     doc: str
@@ -107,6 +154,13 @@ class TestDocumentation(DocumentationType):
 
 @dataclass
 class SectionDocumentation(TestDocumentation):
+    """
+    This class provides support for documenting
+    CMakeTest test sections. It translates the
+    section exactly as :class:`TestDocumentation`
+    except that the warning says that it is a test
+    section.
+    """
 
     def process(self, writer: RSTWriter):
         d = writer.directive(
@@ -120,6 +174,17 @@ class SectionDocumentation(TestDocumentation):
 
 @dataclass
 class MethodDocumentation(DocumentationType):
+    """
+    This is a special documentation type that holds
+    information on a class's method or constructor.
+
+    Its :method:`process` method requires a directive
+    created by :class:`ClassDocumentation`.
+
+    It translates the CMakePP method definition into
+    a Sphinx :code:`py:method` directive, with a note
+    if it is a macro.
+    """
     parent_class: str
     name: str
     param_types: list[str]
@@ -128,7 +193,7 @@ class MethodDocumentation(DocumentationType):
     doc: str
     is_macro: bool = False
 
-    def process(self, writer: RSTWriter):
+    def process(self, writer: Directive):
         params_pretty = ', '.join(
             self.params) + ("[, ...]" if "args" in self.param_types else "")
         d = writer.directive(
@@ -152,12 +217,21 @@ class MethodDocumentation(DocumentationType):
 
 @dataclass
 class AttributeDocumentation(DocumentationType):
+    """
+    This is a special documentation type that stores
+    information on a class's attributes. Its :method:`process`
+    method takes a directive created by :class:`ClassDocumentation`.
+
+    It translates the CMakePP attribute definition into a Sphinx
+    :code:`py:attribute` directive, with a :code:`value` option if
+    it has a default value.
+    """
     parent_class: str
     name: str
     default_value: str
     doc: str
 
-    def process(self, writer: RSTWriter):
+    def process(self, writer: Directive):
         d = writer.directive("py:attribute", f"{self.name}")
         if self.default_value is not None:
             d.option("value", self.default_value)
@@ -167,6 +241,18 @@ class AttributeDocumentation(DocumentationType):
 
 @dataclass
 class ClassDocumentation(DocumentationType):
+    """
+    This is a documentation type that represents a CMakePP
+    class definition. It holds information on all the class's
+    constructors, methods, and attributes via :class:`MethodDocumentation`
+    and :class:`AttributeDocumentation`. It also contains string identifiers
+    for its superclasses and inner classes.
+
+    This class translates the associated CMakePP class definition into a
+    Sphinx :code:`py:class` directive, then loops over all constructors,
+    methods, and attributes and calls their :method:`process` method
+    to add their information to the class directive.
+    """
     name: str
     superclasses: list[str]
     inner_classes: list
