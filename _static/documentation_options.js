20894a241039ec2c7ff21fea61a2d0072f7f746c
ryanmrichard <ryanmrichard@users.noreply.github.com>
2022-08-13 19:38:15 +0000

    deploy: 3865513d404ed06f6bf863d0c8f54001620d7d4f

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'v0.1.4',
+    VERSION: 'v1.0.0',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
