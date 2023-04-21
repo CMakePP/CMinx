9422ee648524c2efa46dc5a5f3ab6def97dc7fd6
Branden Butler <bwtbutler@hotmail.com>
2023-04-21 18:35:31 -0500

    Add additional RST to commented_out_function test

--- tests/test_samples/commented_out_function.cmake
+++ tests/test_samples/commented_out_function.cmake
@@ -5,12 +5,22 @@
 
 #[[[
 # Properly documented command
+#
+# :param param1: This is a parameter
+# :type param1: str
+# :param param2: This is another parameter
+# :type param2: desc
 #]]
-function(test1)
+function(test1 param1 param2)
 endfunction()
 #[[[
 # Documenting a commented-out function should not blow up
+#
+# :param param3: This is a parameter
+# :type param3: str
+# :param param4: This is another parameter
+# :type param4: desc
 #]]
-#function(test)
+#function(test param3 param4)
 #
 #endfunction()
--- tests/test_samples/corr_rst/commented_out_function.rst
+++ tests/test_samples/corr_rst/commented_out_function.rst
@@ -9,10 +9,20 @@ test_samples.commented_out_function
    
 
 
-.. function:: test1()
+.. function:: test1(param1 param2)
 
    Properly documented command
    
+   :param param1: This is a parameter
+   :type param1: str
+   :param param2: This is another parameter
+   :type param2: desc
+   
 
 Documenting a commented-out function should not blow up
+
+:param param3: This is a parameter
+:type param3: str
+:param param4: This is another parameter
+:type param4: desc
 
