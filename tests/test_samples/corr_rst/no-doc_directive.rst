31814cfcfcfc6e1b9378b1d7d276b2e0d84d5c87
JackNoordhuis <jackjnoordhuis@gmail.com>
2024-07-16 14:20:38 +1000

    test/test_samples: add `:cminx: no-doc` directive test

---
+++ tests/test_samples/corr_rst/no-doc_directive.rst
@@ -0,0 +1,6 @@
+
+##################################
+test_samples.no-doc_directive
+##################################
+
+.. module:: test_samples.no-doc_directive
---
+++ tests/test_samples/no-doc_directive.cmake
@@ -0,0 +1,15 @@
+#[[
+# This is an undocumented function
+#
+# :cminx: no-doc
+#]]
+function(undocumented_function)
+endfunction()
+
+#[[
+# This is an undocumented macro
+#
+# :cminx: no-doc
+#]]
+macro(undocumented_macro)
+endmacro()
